6445a23b7e43969b5779f2c4751e93686e717e79
Erwan Richard <erwann.richard@gmail.com>
2014-12-17 18:00:11 +0100

    task order

--- lib/capistrano/lephare.rb
+++ lib/capistrano/lephare.rb
@@ -4,9 +4,6 @@ load File.expand_path("../tasks/apc.rake", __FILE__)
 # Librato tasks
 load File.expand_path("../tasks/librato.rake", __FILE__)
 
-# Deploy tasks
-load File.expand_path("../tasks/deploy.rake", __FILE__)
-
 # Database tasks
 load File.expand_path("../tasks/database.rake", __FILE__)
 
@@ -15,6 +12,9 @@ load File.expand_path("../tasks/rollbar.rake", __FILE__)
 
 # Crontab tasks
 load File.expand_path("../tasks/crontab.rake", __FILE__)
+
+# Deploy tasks
+load File.expand_path("../tasks/deploy.rake", __FILE__)
 
 namespace :load do
   task :defaults do
