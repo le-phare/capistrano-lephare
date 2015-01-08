d16b224ecc2848e1384fafd4ed848a264cdec576
Erwan Richard <erwann.richard@gmail.com>
2015-01-08 09:57:37 +0100

    Fix path

--- lib/capistrano/lephare.rb
+++ lib/capistrano/lephare.rb
@@ -1,5 +1,5 @@
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
-Dir.glob('../tasks/*.rake').each { |r| load r }
+Dir.glob('./tasks/*.rake').each { |r| load r }
 
 namespace :load do
   task :defaults do
