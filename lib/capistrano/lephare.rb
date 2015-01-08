2ab5113e2729d852dc57d6eef42997c7ab9221c6
Erwan Richard <erwann.richard@gmail.com>
2015-01-08 09:58:33 +0100

    Fix task loading

--- lib/capistrano/lephare.rb
+++ lib/capistrano/lephare.rb
@@ -1,5 +1,23 @@
-# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
-Dir.glob('./tasks/*.rake').each { |r| load r }
+# APC tasks
+load File.expand_path("../tasks/apc.rake", __FILE__)
+
+# Librato tasks
+load File.expand_path("../tasks/librato.rake", __FILE__)
+
+# Database tasks
+load File.expand_path("../tasks/database.rake", __FILE__)
+
+# Rollbar tasks
+load File.expand_path("../tasks/rollbar.rake", __FILE__)
+
+# Crontab tasks
+load File.expand_path("../tasks/crontab.rake", __FILE__)
+
+# Deploy tasks
+load File.expand_path("../tasks/deploy.rake", __FILE__)
+
+# SSH tasks
+load File.expand_path("../tasks/ssh.rake", __FILE__)
 
 namespace :load do
   task :defaults do
