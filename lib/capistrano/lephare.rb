48096a80b1d52cb866c6b9d38862cc88e4096d55
Erwan Richard <erwann.richard@gmail.com>
2015-03-02 11:08:16 +0100

    Shorter commande for ssh connection

--- lib/capistrano/lephare.rb
+++ lib/capistrano/lephare.rb
@@ -1,23 +1,11 @@
-# APC tasks
 load File.expand_path("../tasks/apc.rake", __FILE__)
-
-# Librato tasks
 load File.expand_path("../tasks/librato.rake", __FILE__)
-
-# Database tasks
 load File.expand_path("../tasks/database.rake", __FILE__)
-
-# Rollbar tasks
 load File.expand_path("../tasks/rollbar.rake", __FILE__)
-
-# Crontab tasks
 load File.expand_path("../tasks/crontab.rake", __FILE__)
-
-# Deploy tasks
 load File.expand_path("../tasks/deploy.rake", __FILE__)
-
-# SSH tasks
 load File.expand_path("../tasks/ssh.rake", __FILE__)
+load File.expand_path("../tasks/oceanet.rake", __FILE__)
 
 namespace :load do
   task :defaults do
--- lib/capistrano/tasks/ssh.rake
+++ lib/capistrano/tasks/ssh.rake
@@ -1,10 +1,8 @@
-namespace :ssh do
-    desc "Open a ssh session on remote server"
-    task :open do
-        on roles(:app) do |server|
-            run_locally do
-                exec "ssh #{server.user}@#{server}"
-            end
+desc "Open a ssh session on remote server"
+task :ssh do
+    on roles(:app) do |server|
+        run_locally do
+            exec "ssh #{server.user}@#{server}"
         end
     end
 end
